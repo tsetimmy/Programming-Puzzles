a5d1e327dff85ea5ea64c588905284b2365c5827
Timmy Tse <tsetimmy@pink.lwlab.di.utoronto.ca>
2015-06-19 15:47:27 -0400

    no message

--- 2014/1B/c2/c.cpp
+++ 2014/1B/c2/c.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <sstream>
+#include <cassert>
 using namespace std;
 
 typedef long long ll;
@@ -9,9 +11,120 @@ struct z {
   ll parent;
 };
 z zip[dim];
+z zip_copy[dim];
 ll verticies[dim][dim];
 string ans;
 
+string tostring (ll integer) {
+  string ret;
+  ostringstream convert;
+  convert << integer;
+  ret = convert.str();
+  return ret;
+}
+
+ll get (ll idx) {
+  for (ll i = 0; i < dim; i++) {
+    if (verticies[idx][i] != -1 && zip[i].code != -1 && zip[i].status == 0) {
+      goto NEX;
+    }
+  }
+  return -1;
+
+  NEX:
+  ll bidx;
+  for (ll i = 0; i < dim; i++)
+    if (verticies[idx][i] != -1 && zip[i].code != -1 && zip[i].status == 0) {
+      bidx = i;
+      break;
+    }
+  for (ll i = 0; i < dim; i++)
+    if (verticies[idx][i] != -1 && zip[i].code != -1 && zip[i].status == 0 && zip[i].code < zip[bidx].code)
+      bidx = i;
+  return bidx;
+}
+
+ll get_next (ll curr) {
+  for (ll i = 0; i < dim; i++)
+    if (zip[i].code != -1 && zip[i].status == 0)
+      goto NEXT;
+  return -1;
+
+  NEXT:
+  ll bidx;
+  for (ll i = 0; i < dim; i++)
+    if (zip[i].code != -1 && zip[i].status == 0) {
+      bidx = i;
+      break;
+    }
+  for (ll i = 0; i < dim; i++)
+    if (zip[i].code != -1 && zip[i].status == 0 && zip[i].code < zip[bidx].code)
+      bidx = i;
+  return bidx;
+}
+
+void recurse_check (ll idx) {
+  assert(zip_copy[idx].code != -1);
+  assert(zip_copy[idx].status != 2);
+  zip_copy[idx].status = 2;
+  for (ll i = 0; i < dim; i++)
+    if (verticies[idx][i] != -1 && zip_copy[i].status != 2) {
+      assert(zip_copy[i].code != -1);
+      recurse_check(i);
+    }
+}
+
+bool check_connected (ll curr) {
+  for (ll i = 0; i < dim; i++) {
+    zip_copy[i].code = zip[i].code;
+    zip_copy[i].status = zip[i].status;
+    zip_copy[i].parent = zip[i].parent;
+  }
+  recurse_check(curr);
+  for (ll i = 0; i < dim; i++)
+    if (zip_copy[i].code != -1 && zip_copy[i].status != 2)
+      return false;
+  return true;
+}
+
+void recurse (ll idx, ll p) {
+  ans += tostring(zip[idx].code);
+  zip[idx].status++;
+
+  ll next;
+  while ((next = get_next(idx)) != -1) {
+    bool flag = false;
+    for (ll i = 0; i < dim; i++)
+      if (verticies[idx][i] == 1 && i == next) {
+        recurse(i, idx);
+        flag = true;
+        break;
+      }
+    if (p == -1 && flag == false) {
+      ll n;
+      while ((n = get(idx)) != -1) {
+        recurse(n, idx);
+      }
+    } else {
+      if (flag == false) {
+        ll curr_status = zip[idx].status;
+        zip[idx].status = 2; // DEAD status
+        bool connected = check_connected(p);
+        zip[idx].status = curr_status;
+        if (connected) {
+          zip[idx].status = 2;
+          return;
+        } else {
+          ll n;
+          while ((n = get(idx)) != -1) {
+            recurse(n, idx);
+          }
+        }
+      }
+    }
+  }
+  zip[idx].status = 2;
+}
 
 void initialize () {
   ans = "";
@@ -40,15 +153,16 @@ void doit (ll cas) {
 
   ll bestidx;
   for (ll i = 0; i < dim; i++)
-    if (zip[i] != -1) {
+    if (zip[i].code != -1) {
       bestidx = i;
       break;
     }
   for (ll i = 0; i < dim; i++)
-    if (zip[i] != -1 && zip[i] < zip[bestidx])
-      bestidx = i;
+    if (zip[i].code != -1 && zip[i].code < zip[bestidx].code)
+     bestidx = i;
 
-  recurse(bestidx);
+  recurse(bestidx, -1);
+  cout << "Case #" << cas << ": " << ans << endl;
 }
 
 int main () {
