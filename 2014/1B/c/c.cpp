fdf2803626eb5376980d811b3a637c777bdedb8e
tsetimmy <timmy.tse1@gmail.com>
2015-06-20 15:30:14 -0400

    2014/1B/c

--- 2014/1B/c/c.cpp
+++ 2014/1B/c/c.cpp
@@ -1,19 +1,76 @@
+// Works for both small and large inputs.
 #include <iostream>
-#include <vector>
-#include <algorithm>
 #include <sstream>
+#include <cassert>
+#include <queue>
 using namespace std;
 
 typedef long long ll;
-#define dim 55
-ll vertices[dim];
-ll edges[dim][dim];
-ll visited[dim];
+#define dim 1330
+#define sixdig 999999
+ll zip[dim];
+ll cp[dim];
+ll cp2[dim];
+ll status[dim];
+ll verticies[dim][dim];
+string ans;
+ll visited_count;
+ll M, N;
 
-ll N, M;
+void recurse_connected (ll idx) {
+  for (ll i = 0; i < dim; i++) {
+    if (verticies[idx][i] == 1 && cp2[i] != 2) {
+      cp2[i] = 2;
+      recurse_connected(i);
+    }
+  }
+}
 
-bool foo (pair<ll,ll> a, pair<ll,ll> b) {
-  return a.first < b.first;
+bool connected (ll idx) {
+  for (ll i = 0; i < dim; i++) {
+    cp2[i] = cp[i];
+  }
+  recurse_connected(idx);
+
+  for (ll i = 0; i < dim; i++)
+    if (zip[i] != sixdig && cp2[i] == 0) {
+      return false;
+    }
+  return true;
+}
+
+ll next_idx (ll idx) {
+  if (!connected(idx)) {
+    return -1;
+  }
+
+  ll bidx = -1;
+  bool first = true;
+  for (ll i = 0; i < dim; i++) {
+    if (verticies[idx][i] == 1 && cp[i] == 0) {
+      if (first) {
+        bidx = i;
+        first = false;
+      } else if (zip[i] < zip[bidx]) {
+        bidx = i;
+      }
+    }
+  }
+  return bidx;
+}
+
+vector<ll> get_vector (vector<ll> q) {
+  vector<ll> ret;
+  for (ll i = 0; i < dim; i++)
+    cp[i] = status[i];
+  for (ll i = 0; i < q.size(); i++)
+    ret.push_back(-1);
+  for (ll i = q.size() - 1; i >= 0; i--) {
+    if (i != q.size() - 1)
+      cp[q[i + 1]] = 2;
+    ret[i] = next_idx(q[i]);
+  }
+  return ret;
 }
 
 string tostring (ll integer) {
@@ -24,66 +81,69 @@ string tostring (ll integer) {
   return ret;
 }
 
-void clearvisit () {
-  for (ll i = 0; i < dim; i++)
-    visited[i] = 0;
-}
-
-string recurse (ll idx, string s) {
-  visited[idx] = 1;
-  s += tostring(vertices[idx]);
-
-  vector<pair<ll,ll> > tmp;
+void initialize () {
+  visited_count = 0;
+  ans = "";
   for (ll i = 0; i < dim; i++) {
-    if (edges[idx][i] == 1 && visited[i] == 0) {
-      tmp.push_back(make_pair(i, vertices[i]));
-    }
-  }
-  sort(tmp.begin(), tmp.end(), foo);
-  string s2 = s;
-  for (ll i = 0; i < tmp.size(); i++) {
-    if (visited[tmp[i].first] == 0)
-      //cout << idx << " --> " << tmp[i].first << endl;
-      s2 = recurse(tmp[i].first, s2);
-  }
-  return s2;
-}
-
-void doit (ll cas) {
-  for (ll i = 0; i < dim; i++) {
-    vertices[i] = 0;
-    visited[i] = 0;
+    zip[i] = sixdig;
+    status[i] = 0;
   }
   for (ll i = 0; i < dim; i++)
     for (ll j = 0; j < dim; j++)
-      edges[i][j] = 0;
-  cin >> N >> M;
+      verticies[i][j] = -1;
+}
 
-  ll n, v1, v2;
+void doit (ll cas) {
+  initialize();
+  cin >> M >> N;
+  for (ll i = 0; i < M; i++)
+    cin >> zip[i + 1];
+  ll t1, t2;
   for (ll i = 0; i < N; i++) {
-    cin >> n;
-    vertices[i + 1] = n;
-  }
-  for (ll i = 0; i < M; i++) {
-    cin >> v1 >> v2;
-    //cout << v1 << " " << v2 << endl;
-    edges[v1][v2] = 1;
-    edges[v2][v1] = 1;
+    cin >> t1 >> t2;
+    verticies[t1][t2] = 1;
+    verticies[t2][t1] = 1;
   }
 
-  string best = ":";
-  string tmp;
-  for (ll i = 0; i < dim; i++) {
-    if (vertices[i] != 0) {
-      clearvisit();
-      tmp = recurse(i, "");
-      if (tmp < best) {
-        best = tmp;
+  ll curr_idx = 0;
+  for (ll i = 0; i < dim; i++)
+    if (zip[i] < zip[curr_idx])
+      curr_idx = i;
+
+  vector<ll> q;
+  q.push_back(curr_idx);
+  status[curr_idx]++;
+  ans += tostring(zip[curr_idx]);
+  visited_count++;
+  while (visited_count < M) {
+    curr_idx = q.back();
+    vector<ll> b = get_vector(q);
+
+    bool first = true;
+    ll bidx;
+    ll nidx;
+    for (ll i = b.size() - 1; i >= 0; i--) {
+      if (b[i] != -1 && first) {
+        bidx = i;
+        nidx = b[i];
+        first = false;
+      } else if (b[i] != -1 && !first && zip[b[i]] < zip[nidx]) {
+        bidx = i;
+        nidx = b[i];
       }
     }
-  }
-  cout << "Case #" << cas << ": " << best << endl;
 
+    ll sz = b.size();
+    for (ll i = 0; i < sz - 1 - bidx; i++) {
+      status[q.back()] = 2;
+      q.pop_back();
+    }
+    q.push_back(nidx);
+    status[nidx]++;
+    ans += tostring(zip[nidx]);
+    visited_count++;
+  }
+  cout << "Case #" << cas << ": " << ans << endl;
 }
 
 int main () {
