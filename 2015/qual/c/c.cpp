5630ba9931013400f91e7f443107d119402e639a
tim <tim@debian>
2015-07-20 22:48:27 -0400

    2015

--- 2015/qual/c/c.cpp
+++ 2015/qual/c/c.cpp
@@ -1,5 +1,7 @@
+// Works for both small and large inputs.
 #include <iostream>
 #include <cmath>
+#include <cassert>
 using namespace std;
 
 typedef long long ll;
@@ -14,9 +16,48 @@ ll m[5][5] = { {0, 0, 0, 0, 0},
                 {0, 3, -4, -1, 2},
                 {0, 4, 3, -2, -1} };
 
-void doit (ll cas) {
-  cout << "Case #" << cas << ": ";
-  cin >> L >> X >> s;
+ll power (ll val, ll p) {
+  if (p == 1) {
+    return abs(val);
+  } else if (p % 2 == 0) {
+    ll i = power(val, p / 2);
+    return abs(m[i][i]);
+  } else {
+    ll i = power(val, (p - 1) / 2);
+    i = abs(m[i][i]);
+    return abs(m[val][i]);
+  }
+}
+
+bool possible2 () {
+  ll val = 1;
+  ll sgn = 1;
+  for (ll i = 0; i < L; i++) {
+    val = sgn * m[val][(ll)(s[i] - 'g')];
+    sgn = (val > 0) ? 1 : -1;
+    val = abs(val);
+  }
+
+  ll sgn2 = sgn;
+  if (X % 2 == 0)
+    sgn2 = 1;
+
+  ll sgn3 = 1;
+  if (val == 2 || val == 3 || val == 4) {
+    ll mod = X % 4;
+    if (mod == 2 || mod == 3)
+      sgn3 = -1;
+  }
+  //assert(val == 2 || val == 3 || val == 4);
+  val = abs(power(val, X));
+
+
+  sgn = sgn2 * sgn3;
+  //cout << val << " " << sgn << endl;
+  return (val == 1 && sgn == -1);
+}
+
+bool possible () {
   ll val = 1;
   ll sgn = 1;
   for (ll i = 0; i < L * X; i++) {
@@ -25,13 +66,40 @@ void doit (ll cas) {
     sgn = (val > 0) ? 1 : -1;
     val = abs(val);
   }
-  if (!(val == 1 && sgn == -1)) {
+
+  //assert(val == 2 || val == 3 || val == 4);
+  /*
+  val = power(val, X);
+
+  ll sgn2 = sgn;
+  if (X % 2 == 0)
+    sgn2 = 1;
+
+  ll sgn3 = 1;
+  if (val == 2 || val == 3 || val == 4) {
+    ll mod = X % 4;
+    if (mod == 2 || mod == 3)
+      sgn3 = -1;
+  }
+
+  sgn = sgn2 * sgn3;
+  */
+  //cout << val << " " << sgn << endl;
+  return (val == 1 && sgn == -1);
+}
+
+void doit (ll cas) {
+  cout << "Case #" << cas << ": ";
+  cin >> L >> X >> s;
+
+  //possible2();
+  if (!possible2()) {
     cout << "NO" << endl;
     return;
   }
 
-  sgn = 1;
-  val = 1;
+  ll sgn = 1;
+  ll val = 1;
   for (ll i = 0; i < L * X; i++) {
     ll j = i % s.size();
     val = sgn * m[val][(ll)(s[j] - 'g')];
@@ -39,34 +107,17 @@ void doit (ll cas) {
     val = abs(val);
 
     if (val == 2 && sgn == 1) {
-      cout << "found i" << endl;
       ll val2 = 1;
-      cout << "execute" << endl;
       ll sgn2 = 1;
-      cout << "execute2" << endl;
-      ll counter = 0;
       for (ll i2 = i + 1; i2 < L * X; i2++) {
-        cout << sgn2 << endl;
         ll j2 = i2 % s.size();
-        counter++;
-        if (counter == 10) return;
-        cout << "---------------------" << endl;
-        cout << val2 << endl;
-        cout << sgn2 << endl;
-        cout << s[j2] << endl;
         val2 = sgn2 * m[val2][(ll)(s[j2] - 'g')];
-        cout << val2 << endl;
-        ll sgn2 = (val2 > 0) ? 1 : -1;
-        cout << sgn2 << endl;
-        //val2 = abs(val2);
-        val2 = (val2 > 0) ? val2 : -val2;
-        cout << sgn2 << endl;
+        sgn2 = (val2 > 0) ? 1 : -1;
+        val2 = abs(val2);
         if (val2 == 3 && sgn2 == 1) {
           cout << "YES" << endl;
           return;
         }
-        cout << sgn2 << endl;
-        cout << "dude" << endl;
       }
     }
   }
@@ -81,4 +132,3 @@ int main () {
   }
   return 0;
 }
-
