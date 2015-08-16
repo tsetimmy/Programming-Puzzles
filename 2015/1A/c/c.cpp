e3e3a9dcdd545c0d7319306e782ef627865cb01a
tim <tim@debian>
2015-08-16 14:08:28 -0400

    1A/c

--- 2015/1A/c/c.cpp
+++ 2015/1A/c/c.cpp
@@ -1,116 +1,46 @@
+// Works for both small and large inputs.
 #include <iostream>
-#include <vector>
 #include <algorithm>
-#include <limits>
 #include <cmath>
-#include <cstdio>
 using namespace std;
 
-
-#define EP 1e-15
-long double inf = std::numeric_limits<double>::infinity();
-
-struct pt {
-  int x;
-  int y;
-  long double angle;
-  int best;
-  int idx;
-};
-
-int N;
-vector<pt> pts;
-
-void inc (int & a) {
-  a++;
-  a = a % pts.size();
-}
-
-pt vectorize (pt a, pt b) {
-  pt c;
-  c.x = a.x - b.x;
-  c.y = a.y - b.y;
-  return c;
-}
-
-bool foo (pt a, pt b) {
-  return (a.angle <= b.angle);
-}
-
-bool foo2 (pt a, pt b) {
-  return (a.idx <= b.idx);
-}
-
-int cross (pt u, pt v, int d) {
-  return (u.x * v.y - u.y * v.x);
-}
-
-void remove_negatives () {
-  int i = 0;
-  while (i < pts.size()) {
-    if (pts[i].idx == -1) {
-      pts.erase(pts.begin() + i);
-    } else {
-      i++;
-    }
-  }
-}
+long double ep = 1e-15;
 
 void doit (int cas) {
+  int n;
+  cin >> n;
+  int x[n], y[n];
+
+  for (int i = 0; i < n; i++) {
+    cin >> x[i] >> y[i];
+  }
   cout << "Case #" << cas << ":" << endl;
-  cin >> N;
-  pts.clear();
-  pts = vector<pt> (N);
 
-  for (int i = 0; i < N; i++) {
-    cin >> pts[i].x;
-    cin >> pts[i].y;
-    pts[i].best = 3001;
-    pts[i].idx = i;
-  }
-
-  if (N <= 4) {
-    for (int i = 0; i < N; i++)
-      cout << 0 << endl;
-    return;
-  }
-
-
-  for (int p = 0; p < pts.size(); p++) {
-    for (int q = 0; q < pts.size(); q++) {
-      if (p != q) {
-
-        pt pt_p = pts[p];
-        pt pt_q = pts[q];
-
-        pts[p].idx = -1;
-        pts[q].idx = -1;
-        remove_negatives();
-
-        for(int i = 0; i < pts.size(); i++)
-          pts[i].angle = atan2l(pts[i].y - pt_p.y, pts[i].x - pt_p.x);
-
-        pt phead, ptail;
-        int h, t;
-
-
-
+  for (int i = 0; i < n; i++) {
+    long double a[2 * n];
+    int ans = 30000, an = 0;
+    for (int j = 0; j < n; j++) {
+      if (i != j) {
+        a[an] = atan2l(y[j] - y[i], x[j] - x[i]);
+        an++;
       }
     }
+    sort(a, a + an);
+    for (int j = 0; j < an; j++) {
+      a[an + j] = a[j] + 2 * M_PI;
+    }
+
+    int l = 0, r = 0;
+    for (int j = 0; j < n; j++) {
+      while (l + 1 < 2 * an && a[l] - a[j] < ep)
+        l++;
+      while (r < 2 * an && a[r] - a[j] < M_PI - ep)
+        r++;
+      ans = min(ans, r - l);
+    }
+    cout << ans << endl;
   }
-
-  sort(pts.begin(), pts.end(), foo2);
-  for (int i = 0; i < pts.size(); i++)
-    cout << pts[i].angle << endl;
-
-
-
-
-
-
-
 }
-
 
 int main () {
   int tcases;
@@ -119,4 +49,3 @@ int main () {
     doit(cas + 1);
   return 0;
 }
-
